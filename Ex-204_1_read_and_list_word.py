71a0f999e2744458c231ccb3ef139fe5f32ddf67
masrik-dev <111740924+masrik-dev@users.noreply.github.com>
2025-05-04 23:59:35 +0600

    --update ex-204_1

---
+++ Ex-204_1_read_and_list_word.py
@@ -0,0 +1,19 @@
+'''
+read a file "romeo.txt" and read it. then count words and list them in alphabetical order.
+
+'''
+
+file_name = input("Enter file name: ")
+file_handel = open(file_name)
+list_words = list()
+for lines in file_name:
+    line = lines.rstrip()
+    words = line.split()
+    for word in words:
+        if word in list_words:
+            continue
+        else:
+            list_words.append(word)
+
+list_words.sort()
+print(list_words)
