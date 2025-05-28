871514c8669b16e8560ff2c9e523410a37420801
masrik-dev <111740924+masrik-dev@users.noreply.github.com>
2025-05-28 23:24:34 +0600

    --update ex-301

---
+++ Ex-300/Ex-301_2.py
@@ -0,0 +1,12 @@
+import re
+
+filename = input("Enter file name: ")
+f = open(filename)
+d = f.read()
+y = re.findall('[0-9]+', d)
+sum = 0
+for i in range(len(y)):
+    c = int(y[i])
+    sum += c
+
+print(sum) 
