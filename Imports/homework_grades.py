a00ac6f9698da3310f4307cd33175c6a0134cd51
masrik-dev <111740924+masrik-dev@users.noreply.github.com>
2026-03-09 01:08:42 +0600

    --python refreser day 1

--- Imports/homework_grades.py
+++ Imports/homework_grades.py
@@ -5,18 +5,14 @@
     clean maintainable code.
 """
 
+import Imports.grade_average_service
+
 homework_assignment_grades = {
     'homework_1': 85,
     'homework_2': 100,
     'homework_3': 81
 }
 
-def calculate_homework(homework_assignments_arg):
-    sum_of_grades = 0
-    for homework in homework_assignments_arg.values():
-        sum_of_grades += homework
-    final_grade = round(sum_of_grades / len(homework_assignments_arg), 2)
-    print(final_grade)
 
 
 calculate_homework(homework_assignment_grades)
