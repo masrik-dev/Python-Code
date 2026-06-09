ce71ae2f9058303e9b4bdfb67afe2dfc4247f930
masrik-dev <111740924+masrik-dev@users.noreply.github.com>
2026-06-09 23:53:40 +0600

    --update test.py

--- test.py
+++ test.py
@@ -30,3 +30,23 @@ def find_highest_lowest(homework_assignments_arg):
     print(f"Lowest Grade: {lowest_grade}")
 
 find_highest_lowest(homework_assignment_grades)
+
+import random
+student_grades = {}
+
+for student_num in range(1, 21):
+    student_grades[f"student_{student_num}"] = random.randint(40, 100)
+
+for student, grade in student_grades.items():
+    print(f"{student}: {grade}")
+
+sum_of_grades = sum(student_grades.values())
+average_grade = round(sum_of_grades/len(student_grades), 2)
+
+highest_grade = max(student_grades.values())
+lowest_grade = min(student_grades.values())
+
+highest_student = max(student_grades, key=student_grades.get)
+lowest_student = min(student_grades, key=student_grades.get)
+
+print(f"Summary:\nAverage Grade: {average_grade}\nHigest Grade: {highest_grade} ({highest_student})\nLowest Grade: {lowest_grade} ({lowest_student})")
